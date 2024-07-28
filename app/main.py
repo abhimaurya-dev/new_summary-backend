e699683dae25acb4287075614d29f53430726bd2
Abhishek Maurya <abhishekmaurya.official@gmail.com>
2024-07-28 11:55:07 +0530

    first commit and added data_cleaning.py

---
+++ app/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from starlette.middleware.cors import CORSMiddleware
+import numpy as np
+import pickle
+import nltk
+from app.utils.data_cleaning import data_cleaning
+
+
+app = FastAPI()
+
+nltk.download('wordnet')
+nltk.download('punkt')
+nltk.download('omw-1.4')
+# origins = ["https://soft-klepon-49df2a.netlify.app", "*"]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+@app.get("/", response_class=HTMLResponse)
+def root():
+    return """<html>
+              <head>
+                <title>Home</title>
+              </head>
+              <body style='overflow:hidden; background-color:grey'>
+                <h1 style='display:flex; align-items:center; color:white; justify-content:center; width:100vw; font-size:4rem; height:100vh'>Welcome To News Summarizer Backend API</h1>
+              </body>
+              </html>"""
+
+
+
