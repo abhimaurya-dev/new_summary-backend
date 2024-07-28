f696f93ac961dfed2234abc37a8ac0ef15552f52
Abhishek Maurya <abhishekmaurya.official@gmail.com>
2024-07-28 18:30:05 +0530

    added model remotely and removed model code from local

--- app/routers/get_summary.py
+++ app/routers/get_summary.py
@@ -1,5 +1,9 @@
-from transformers import pipeline
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
+
+model_name = "abhimaurya-dev/t5-news"
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 def get_summary():
-  pipe = pipeline("summarization", model="t5-news/checkpoint-792", tokenizer="tokenizer")
+  pipe = pipeline("summarization", model=model, tokenizer=tokenizer)
   print(pipe("hello this is first line"))
