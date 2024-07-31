e1649a2f3930c9629f662a5df8747aea63dcba2e
Abhishek Maurya <abhishekmaurya.official@gmail.com>
2024-07-31 12:16:44 +0530

    Update .env for deployment

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT")) 
     print(f"Starting server on port {port}")
     uvicorn.run(app, host="0.0.0.0", port=port)
